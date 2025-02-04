0f38da8ae6055ba60b6cac567d873882bc5aaf6d
root <root@DESKTOP-JDEMI8P.localdomain>
2025-02-04 10:10:55 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,61 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_linev2.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbanzo-s <fbanzo-s@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/27 20:56:57 by fbanzo-s          #+#    #+#             */
-/*   Updated: 2025/01/27 20:56:57 by fbanzo-s         ###   ########.fr       */
+/*   Created: 2025/02/02 14:56:40 by fbanzo-s          #+#    #+#             */
+/*   Updated: 2025/02/02 14:56:40 by fbanzo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_updatestored(char *stored, int i)
-{
-	char	*new;
-
-	if (stored[i] == '\n')
-		new = ft_strdup(&stored[i + 1]);
-	else
-		new = NULL;
-	free(stored);
-	return (new);
-}
-
 char	*ft_getline(char **stored)
 {
-	size_t	i;
-	size_t	l;
-	char	*tmp;
 
-	i = 0;
-	while ((*stored)[i] != '\n' && (*stored)[i])
-		i++;
-	if ((*stored)[i] == '\n')
-		tmp = (char *)malloc(sizeof(char) * (i + 2));
-	else
-		tmp = (char *)malloc(sizeof(char) * (i + 1));
-	l = 0;
-	while (tmp[l] && l < i)
-	{
-		tmp[l] = (*stored)[l];
-		l++;
-	}
-	if ((*stored)[i] == '\n')
-	{
-		tmp[l] = '\n';
-		l++;
-	}
-	tmp[l] = '\0';
-	*stored = ft_updatestored(*stored, i);
-	return (tmp);
 }
 
-static char	*ft_read(int fd, char *stored)
+char	*ft_read(int fd, char *stored)
 {
-	size_t	bytesread;
+	ssize_t	bytesread;
 	char	*buffer;
 
 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
@@ -65,32 +29,17 @@ static char	*ft_read(int fd, char *stored)
 	while (bytesread > 0 && !ft_strchr(buffer, '\n'))
 	{
 		buffer[bytesread] = '\0';
-		stored = ft_strjoin(stored, buffer);
-		if (!stored)
-		{
-			free(buffer);
-			return (NULL);
-		}
 		bytesread = read(fd, buffer, BUFFER_SIZE);
 	}
-	free(buffer);
-	return (stored);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*stored;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	stored = ft_read(fd, stored);
-	if (!stored)
-		return (NULL);
-	if (*stored == '\0')
-	{
-		free(stored);
-		stored = NULL;
-		return (NULL);
-	}
-	return (ft_getline(&stored));
+	ft_read(fd, stored);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -39,11 +39,13 @@ char	*ft_strdup(const char *s)
 {
 	size_t	len;
 	char	*dup;
-	int		i;
+	size_t	i;
 
+	if (!s)
+		return (NULL);
 	len = ft_strlen(s);
-	dup = (char *)malloc(len * sizeof(char) + 1);
-	if (dup == NULL)
+	dup = (char *)malloc(len + 1);
+	if (!dup)
 		return (NULL);
 	i = 0;
 	while (s[i])
@@ -55,9 +57,9 @@ char	*ft_strdup(const char *s)
 	return (dup);
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*s3;
+	char	*joined;
 	size_t	i;
 	size_t	j;
 
@@ -65,22 +67,21 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	s3 = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (!s3)
+	joined = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!joined)
 		return (NULL);
 	i = 0;
 	while (s1[i])
 	{
-		s3[i] = s1[i];
+		joined[i] = s1[i];
 		i++;
 	}
 	j = 0;
 	while (s2[j])
 	{
-		s3[i] = s2[j];
-		i++;
+		joined[i + j] = s2[j];
 		j++;
 	}
-	s3[i] = '\0';
-	return (s3);
+	joined[i + j] = '\0';
+	return (joined);
 }
